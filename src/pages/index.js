93f1b78b05f643dfe4af2a6b9168478e8230e608
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-24 11:07:02 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import LandingPage from "./LandingPage/LandingPage";
+import LoginPage from "./LoginPage/LoginPage";
+import PageNotFound from "./PageNotFound/PageNotFound";
+
+import StudentDashboardPage from "./studentPages/StudentDashBoardPage";
+import ProfessorDashboardPage from "./professorPages/ProfessorDashboardPage";
+
+import ProfessorProposalCreationPage from "./professorPages/ProfessorProposalCreation";
+import ProfessorApplicantsPage from "./ProfessorPages/ProfessorApplicantsPage.jsx";
+
+import DefaultLayoutPage from "./DefaultLayoutPage/DefaultLayoutPage";
+
+import ThesesPage from "./ThesesPage/ThesesPage.jsx";
+
+export {
+    DefaultLayoutPage,
+    LandingPage,
+    LoginPage,
+    PageNotFound,
+
+    StudentDashboardPage,
+    
+    ProfessorDashboardPage,
+    ProfessorProposalCreationPage,
+    ProfessorApplicantsPage,
+
+    ThesesPage,
+}
